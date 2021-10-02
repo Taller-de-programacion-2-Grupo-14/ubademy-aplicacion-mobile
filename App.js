5b25d782507010e9c82bc8cb07afe8b0387748a2
violeta <viperez@fi.uba.ar>
2021-10-02 19:44:00 -0300

    explosion

--- App.js
+++ App.js
@@ -1,202 +1,40 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {
-  NativeBaseProvider,
-  Box,
-  Text,
-  Heading,
-  VStack,
-  FormControl,
-  Input,
-  Link,
-  Button,
-  Icon,
-  IconButton,
-  HStack,
-  Divider,
-  Center,
-  Image
-} from 'native-base';
-import { createServer } from "miragejs";
-import FirstPage from './components/FirstPage/FirstPage';
+import Text from 'native-base';
+import { createStackNavigator } from '@react-navigation/stack';
 
-if (window.server) {
-  server.shutdown()
-}
-
-window.server = createServer({
-  routes() {
-    this.get("/api/cursos", () => {
-      return {
-        cursos: [
-          { id: 1, name: "Data Science", year: 2010 },
-          { id: 2, name: "Docker", year: 2014 },
-          { id: 3, name: "Matematica", year: 2017 },
-        ],
-      }
-    })
-  },
-})
-
-
-function SignIn({ navigation }) {
-  let [cursos, setCursos] = React.useState([])
-
-  React.useEffect(() => {
-    fetch("/api/cursos")
-      .then((res) => res.json())
-      .then((json) => setCursos(json.cursos))
-  }, [])
-
-  return (
-    <NativeBaseProvider>
-      <Box safeArea flex={1} p="2" py="8" w="90%" mx="auto" style={{ justifyContent: 'center'}}>
-        <Center>
-          <Image
-            source={require('./images/logo.png')}
-            alt="Logo"
-            size="xl"
-          />
-        </Center>
-        <Heading size="lg" fontWeight="600" color="coolGray.800">
-          Bienvenido
-        </Heading>
-        <Heading mt="1" color="coolGray.600" fontWeight="medium" size="xs">
-          Iniciar sesion
-        </Heading>
-
-        <VStack space={3} mt="5">
-          <FormControl>
-            <FormControl.Label
-              _text={{
-                color: 'coolGray.800',
-                fontSize: 'xs',
-                fontWeight: 500,
-              }}>
-              Email
-            </FormControl.Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label
-              _text={{
-                color: 'coolGray.800',
-                fontSize: 'xs',
-                fontWeight: 500,
-              }}>
-              Password
-            </FormControl.Label>
-            <Input type="password" />
-            <Link
-              _text={{ fontSize: 'xs', fontWeight: '500', color: 'indigo.500' }}
-              alignSelf="flex-end"
-              mt="1">
-              ¿Olvido su contraseña?
-            </Link>
-          </FormControl>
-          <Button mt="2" colorScheme="indigo" _text={{ color: 'white' }}  >
-            Iniciar sesion
-          </Button>
-            {cursos.map((cursos) => (
-              <Text key={cursos.id}>
-                {cursos.name} ({cursos.year})
-              </Text>
-            ))}
-          <HStack mt="6" justifyContent="center">
-            <Text fontSize="sm" color="muted.700" fontWeight={400}>
-              ¿Usuario nuevo?{' '}
-            </Text>
-            <Link onPress={() => navigation.navigate('SignUp')}
-              _text={{
-                color: 'indigo.500',
-                fontWeight: 'medium',
-                fontSize: 'sm',
-              }}
-            >
-              Registrate
-            </Link>
-          </HStack>
-        </VStack>
-      </Box>
-    </NativeBaseProvider>
-  );
-}
-
-
-
-class SignUp extends React.Component({ navigation }) {
-  constructor (){
-    super();
-    this.state={
-      email:'',
-      password:'',
-    };
-  }
-  submit(){
-    console.warn(this.state)
-  }
+class SignIn extends React.Component{
   render(){
-    return (
-    <NativeBaseProvider>
-      <Box safeArea flex={1} p="2" w="90%" mx="auto" py="8" style={{ justifyContent: 'center'}}>
-        <Center>
-          <Image
-            source={require('./images/logo.png')}
-            alt="Logo"
-            size="xl"
-          />
-        </Center>
-        <Heading size="lg" color="coolGray.800" fontWeight="600">
-          Bienvenido
-        </Heading>
-        <Heading mt="1" color="coolGray.600" fontWeight="medium" size="xs">
-          Registrate para continuar!
-        </Heading>
-
-        <VStack space={3} mt="5">
-          <FormControl>
-            <FormControl.Label
-              _text={{ color: 'muted.700', fontSize: 'xs', fontWeight: 500 }} onChangeText={(text) => { this.setState({ email: text})}}>
-              Email
-            </FormControl.Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label
-              _text={{ color: 'muted.700', fontSize: 'xs', fontWeight: 500 }}>
-              Password
-            </FormControl.Label>
-            <Input type="password" />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label
-              _text={{ color: 'muted.700', fontSize: 'xs', fontWeight: 500 }}>
-              Confirmar Password
-            </FormControl.Label>
-            <Input type="password" />
-          </FormControl>
-          <Button mt="2" colorScheme="indigo" _text={{ color: 'white' }} onPress={() => {this.submit()}}>                                                                                   
-            Registrate
-          </Button>
-        </VStack>
-      </Box>
-    </NativeBaseProvider>
+  return (
+    <Text>Hola Cachorro</Text>
   );
 }}
 
-const Stack = createNativeStackNavigator();
 
-function App() {
+
+class SignUp extends React.Component {
+  render(){
+    return (
+      <Text>Bienvenido Cachorro</Text>
+  );
+}}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        {/* <Stack.Screen name="FirstPage" component={FirstPage} /> */}
+      <Stack.Navigator>
         <Stack.Screen name="Home" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
-    </NavigationContainer>
   );
 }
 
-export default App;
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
+    );
+}
